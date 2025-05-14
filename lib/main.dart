37dab7cfff957c01cb744d95f5cdf9833d07444d
amitsingh1130 <amits979563@gmail.com>
2025-05-14 15:55:28 +0530

    Basic flutter command

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const Amit());
 
 // #docregion my-app
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+class Amit extends StatelessWidget {
+  const Amit({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'amitTitle',
       home: Scaffold(
-        appBar: AppBar(title: const Text('mera Title')),
-
-        body: const Text('App ka body'
+        appBar: AppBar(title: const Text('amit')),
+        // #docregion centered-text
+        body: const Center(
+          // #docregion text
+          child: Text('To path to success is never easy'),
+          // #enddocregion text
         ),
         // #enddocregion centered-text
       ),
