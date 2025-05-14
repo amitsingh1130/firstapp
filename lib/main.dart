6205dcc8a00ee2ac1eee544fa5fe6ca2bf2959cb
amitsingh1130 <amits979563@gmail.com>
2025-05-14 14:42:05 +0530

    Basic fullter

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
+// #docregion my-app
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Center(child: const Text("Hello ")));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('mera Title')),
+
+        body: const Text('App ka body'
+        ),
+        // #enddocregion centered-text
+      ),
+    );
   }
 }
