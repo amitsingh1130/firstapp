8d4ff146d1c279f840a4a5593f029b0108f2e8f9
amitsingh1130 <amits979563@gmail.com>
2025-05-16 09:33:15 +0530

    solution to create two buttons

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,9 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  int n=0;
-   MyApp({super.key});
+  int n = 0;
+  int i = (-1);
+  MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -17,22 +18,45 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: true,
       home: Scaffold(
-        appBar: AppBar(title: Text('Apple  ')),
-        body: Column(children: [Center(child: Text('follow good habits')),
-          ElevatedButton(onPressed: (){
-            print(widget.n);
-            widget.n++;
-            setState(() {
+        appBar: AppBar(title: Text('Page')),
+        body: Column(
+          children: [
+            Center(
+              child: Text(
+                ' ${widget.n}  जीवन एक यात्रा है, मंजिल नहीं।'
+                'हर दिन एक नया अवसर है खुद को बेहतर बनाने का।'
+                'सकारात्मक सोचो, सकारात्मक रहो।'
+                'हर सुबह एक नया आशीर्वाद और एक नया अवसर लेकर आती है।',
+              ),
+            ),
+            Center(
 
-            });
+            child: Text(
+              '${widget.i} by leaps and bounds.'
+            )
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print(widget.n);
+                widget.n++;
+                setState(() {});
+              },
+              child: Text("Click ${widget.n}"),
+            ),
 
-          }, child: Text("Click ${widget.n}")),
-
-
-        ]),
-         ),
-      );
-
+            ElevatedButton(
+              onPressed: () {
+                print(widget.i);
+                widget.i--;
+                setState(() {});
+              },
+              child: Text("Click ${widget.i}"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
