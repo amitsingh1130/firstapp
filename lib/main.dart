2bbee9ca6b9e41f83d0be076eeffb8b4a364f555
amitsingh1130 <amits979563@gmail.com>
2025-05-21 11:59:36 +0530

    put value

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 // import
 
 import 'myapp.dart';
+import 'dart:math';
 
 void main() {
   runApp(Adder());
+  runApp(MaterialApp(home: TriangleCalculator()));
+
+
 }
+
--- lib/myapp.dart
+++ lib/myapp.dart
@@ -34,6 +34,9 @@ class _AdderState extends State<Adder> {
                 print('Second text field: $text2 (${text2.characters.length}');
               },
             ),
+            Center(
+              
+            ),
 
             ElevatedButton(
               onPressed: () {
@@ -62,6 +65,60 @@ class _AdderState extends State<Adder> {
                 setState(() {});
               },
               child: Text("Divided"),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+class TriangleCalculator extends StatefulWidget {
+  @override
+  _TriangleCalculatorState createState() => _TriangleCalculatorState();
+}
+class _TriangleCalculatorState extends State<TriangleCalculator> {
+  final baseController = TextEditingController();
+  final heightController = TextEditingController();
+  double? area;
+
+  double calculateTriangleArea(double base, double height) {
+    return area =1/2 * base * height;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Triangle Area Calculator')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: baseController,
+              decoration: InputDecoration(labelText: 'Enter base'),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              controller: heightController,
+              decoration: InputDecoration(labelText: 'Enter height'),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                double base = double.parse(baseController.text);
+                double height = double.parse(heightController.text);
+                setState(() {
+                  area = calculateTriangleArea(base, height);
+                });
+              },
+              child: Text('Calculate Area'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              area != null ? 'Area: ${area!.toStringAsFixed(2)}' : '',
+              style: TextStyle(fontSize: 18),
             ),
           ],
         ),
