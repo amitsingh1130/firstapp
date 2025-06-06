9f3fa5bb280e205fc780848c540525ab80600655
amitsingh1130 <amits979563@gmail.com>
2025-06-06 18:12:14 +0530

    Nesting Row Inside Column and arrange text.

--- lib/newapp.dart
+++ lib/newapp.dart
@@ -13,30 +13,91 @@ class Newapp extends StatelessWidget {
                 Text("Amit"),
                 Row(
                   children: [
-                    Expanded(child: Container(color: Colors.indigo,height: 100))
-                  ],
-                )
-              ],
-            ),
-            Column(
-              children: [
-                Text("Header"),
-                Row(
-                  children: [
-                    Expanded(child: Container(color: Colors.pinkAccent, height: 100)),
-                    Expanded(child: Container(color: Colors.brown, height: 100)),
+                    Expanded(
+                      child: Container(
+                        child: Center(
+                          child: Text(
+                            "What is the capital of France?",
+                            style: TextStyle(fontSize: 50),
+                          ),
+                        ),
+                        color: Colors.indigo,
+                        height: 120,
+                      ),
+                    ),
                   ],
                 ),
-                Text("Footer"),
               ],
             ),
             Column(
               children: [
                 Row(
                   children: [
-                    Expanded(child: Container(color: Colors.yellow, height: 100)),
-                    Expanded(child: Container(color: Colors.purple, height: 100)),
-                    Expanded(child: Container(color: Colors.orange, height: 100)),
+                    Expanded(
+                      child: Container(color: Colors.pinkAccent, height: 120),
+                    ),
+                    Expanded(
+                      child: Container(color: Colors.brown, height: 120),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(color: Colors.yellow, height: 125),
+                    ),
+                    Expanded(
+                      child: Container(color: Colors.purple, height: 125),
+                    ),
+                    Expanded(
+                      child: Container(color: Colors.orange, height: 125),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: Text(
+                          "A. Berlin",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        color: Colors.pink,
+                        height: 130,
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Text(
+                          "B. Madrid",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        color: Colors.green,
+                        height: 130,
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Text("C. Paris", style: TextStyle(fontSize: 20)),
+                        color: Colors.white,
+                        height: 130,
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Text("D. Rome", style: TextStyle(fontSize: 20)),
+                        color: Colors.grey,
+                        height: 130,
+                      ),
+                    ),
                   ],
                 ),
               ],
