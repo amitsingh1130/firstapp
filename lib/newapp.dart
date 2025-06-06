d05dacd34442686c019884b46ddb7abccad3af73
amitsingh1130 <amits979563@gmail.com>
2025-06-06 17:39:48 +0530

    Nesting Row Inside Column

--- lib/newapp.dart
+++ lib/newapp.dart
@@ -8,93 +8,42 @@ class Newapp extends StatelessWidget {
         appBar: AppBar(title: Text("Demo App")),
         body: Column(
           children: [
-            Container(
-              padding: EdgeInsets.all(7),
-              color: Colors.deepPurple,
-              child: Text(
-                "Header",
-                style: TextStyle(
-                  fontSize: (32),
-                  color: Colors.green,
-                  backgroundColor: Colors.yellowAccent,
+            Column(
+              children: [
+                Text("Amit"),
+                Row(
+                  children: [
+                    Expanded(child: Container(color: Colors.indigo,height: 100))
+                  ],
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Text("Header"),
+                Row(
+                  children: [
+                    Expanded(child: Container(color: Colors.pinkAccent, height: 100)),
+                    Expanded(child: Container(color: Colors.brown, height: 100)),
+                  ],
                 ),
-              ),
+                Text("Footer"),
+              ],
             ),
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(32),
-                      decoration: BoxDecoration(
-                        color: Colors.teal,
-                        borderRadius: BorderRadius.circular(12),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.brown.withOpacity(0.5),
-                            spreadRadius: 3,
-                            blurRadius: 7,
-                            offset: Offset(0, 3),
-                          ),
-                        ],
-                      ),
-                      child: Text(
-                        "AMIT",
-                        style: TextStyle(
-                          backgroundColor: Colors.redAccent,
-                          fontSize: (56),
-                        ),
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      print(Switch);
-                    },
-                    child: Text(
-                      "switch",
-                      style: TextStyle(
-                        color: Colors.teal,
-                        backgroundColor: Colors.yellow,
-                        fontSize: 35,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Expanded(child: Container(color: Colors.greenAccent)),
-                        ElevatedButton(
-                          onPressed: () {
-                            print(Router);
-                          },
-                          child: Text(
-                            "Router",
-                            style: TextStyle(
-                              color: Colors.teal,
-                              backgroundColor: Colors.yellow,
-                              fontSize: 35,
-                            ),
-                          ),
-                        ),
-                        Expanded(child: Container(color: Colors.grey)),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-            Container(
-              padding: EdgeInsets.all(32),
-              color: Colors.yellowAccent,
-              child: Text("Footer", style: TextStyle(fontSize: (32))),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(child: Container(color: Colors.yellow, height: 100)),
+                    Expanded(child: Container(color: Colors.purple, height: 100)),
+                    Expanded(child: Container(color: Colors.orange, height: 100)),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
       ),
     );
   }
-
-  const Newapp({super.key});
 }
